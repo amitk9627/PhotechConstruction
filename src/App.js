2e37f837fc50b239b459bcb7e900b96a51db57c0
Amit kumar <ramitgzp56@gmail.com>
2023-11-01 17:46:41 +0530

    routes change

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path='/home' element={<Home />} />
-        <Route path='/' element={<About />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
         <Route path='/contact' element={<Contact />} />
       </Routes>
      
